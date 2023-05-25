18f2a033da79d0dc7236cde16e93032c2dec9408
Keerty Verma <keertyverma1303@gmail.com>
2023-05-25 17:55:48 +0530

    Create profile page and list all prompt for given user

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -2,12 +2,22 @@
 
 import { useState } from "react";
 import { useSession } from "next-auth/react";
+import { usePathname, useRouter } from "next/navigation";
 import Image from "next/image";
 
-const PromptCard = ({ post, handleTagClick }) => {
+const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const { data: session } = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
+
   const [copied, setCopied] = useState("");
 
-  const { data: session } = useSession();
+  const handleCopy = () => {
+    setCopied(post.prompt);
+    navigator.clipboard.writeText(post.prompt);
+
+    setTimeout(() => setCopied(""), 3000);
+  };
 
   return (
     <div className="prompt_card">
@@ -30,7 +40,7 @@ const PromptCard = ({ post, handleTagClick }) => {
           </div>
         </div>
       </div>
-      <div className="copy_btn" onClick={() => {}}>
+      <div className="copy_btn" onClick={handleCopy}>
         <Image
           src={
             copied === post.prompt
@@ -51,6 +61,22 @@ const PromptCard = ({ post, handleTagClick }) => {
       >
         {post.tag}
       </p>
+      {session?.user.id === post.creator._id && pathName === "/profile" && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+          <p
+            className="font-inter text-sm green_gradient cursor-pointer"
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className="font-inter text-sm orange_gradient cursor-pointer"
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   );
 };
